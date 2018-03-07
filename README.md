9995612c3e2433c976c096b01be81394dd34a605
anveshc05 <anveshc05@gitlab.com>
2018-03-07 14:58:30 +0530

    microtask-1 completed

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Chaoss-Microtasks
+
+* **Requirements**
+1. development environment, python3, pip3
+2. Elasticsearch running on localhost:9200, Kibana running on localhost:5601
+3. Grimoielab packages like sortinghat, perceval; 
+
+#### Micro-task 1
+
+Produce a Python script that produces configuration files for Mordred to analyze a complete GitHub organization, excluding repositories that are forks from other GitHub repositories. Test it with at least two GitHub organizations, producing screenshots of the resulting dashboard.
+
+**Solution file**  
+
+The python code which generates the configuration files which generate the configuration files for mordred can be found in the [mtask1](https://github.com/anveshc05/Chaoss-Microtasks/tree/master/mtask1/) directory by the name [mtask1.py](https://github.com/anveshc05/Chaoss-Microtasks/tree/master/mtask1/mtask1.py).
+
+I have produced dashboards for 2 repositories which are
+* [haiku](https://github.com/haiku)
+* [robocomp](https://github.com/robocomp)
+
+** Screenshots **  
+All the screenshots have been placed in the folder [Screenshots](https://github.com/anveshc05/Chaoss-Microtasks/tree/master/mtask1/Screenshots/) inside the [mtask1](https://github.com/anveshc05/Chaoss-Microtasks/tree/master/mtask1/) directory.
+
+_Some sample screenshots for reference_
+
+![git](https://github.com/anveshc05/Chaoss-Microtasks/tree/master/mtask1/Screenshots/Haiku/haiku_git_full.png)
+
+![pull_requests](https://github.com/anveshc05/Chaoss-Microtasks/tree/master/mtask1/Screenshots/Haiku/github_pull_requests.png)
+
+
+**How To Run**  
+* Clone the repository using
+> git clone https://github.com/anveshc05/Chaoss-Microtasks.git
+* Run command
+> python mtask1.py <Name of organisation>. 
+This would lead to formation of 2 files <orgname.cfg> and <orgname.json>
+* Start elasticearch and kibana.
+* Change the name of sorting-hat user, sorting-hat password and [add your github api token](https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/) to the github backend.
+* Run the command 
+> mordred -c <orgname.cfg>
+* Run kibana to see dashboards.
+
+
---
+++ mtask1/mtask1.py
@@ -0,0 +1,109 @@
+import requests
+import sys
+import json
+import configparser
+
+def create_config(org):
+	config = configparser.ConfigParser()
+	config.add_section('general')
+	config['general']['short_name'] = 'GrimoireLab'
+	config['general']['update'] = 'false'
+	config['general']['sleep'] = '0'
+	config['general']['debug'] = 'true'
+	config['general']['min_update_delay'] = '10'
+	config['general']['logs_dir'] = '/tmp/logs'
+	config['general']['kibana'] = '"5"'
+
+	config.add_section('projects')
+	config['projects']['projects_file'] = org + '.json'
+
+	config.add_section('es_collection')
+	config['es_collection']['url'] = 'http://localhost:9200'
+	config['es_collection']['user'] = ''
+	config['es_collection']['password'] = ''
+
+	config.add_section('es_enrichment')
+	config['es_enrichment']['url'] = 'http://127.0.0.1:9200'
+	config['es_enrichment']['user'] = ''
+	config['es_enrichment']['password'] = ''
+	config['es_enrichment']['autorefresh'] = 'false'	
+	config['es_enrichment']['studies'] = 'true'
+
+	config.add_section('sortinghat')
+	config['sortinghat']['host'] = 'localhost'
+	config['sortinghat']['user'] = 'anv'
+	config['sortinghat']['password'] = 'passpass'
+	config['sortinghat']['database'] = 'shatdb'
+	config['sortinghat']['load_orgs'] = 'false'
+	config['sortinghat']['unify_method'] = ''
+	config['sortinghat']['unaffiliated_group'] = 'Unknown'
+	config['sortinghat']['affiliate'] = 'True'
+	config['sortinghat']['autoprofile'] = '[customer,git,github]'
+	config['sortinghat']['matching'] = '[email]'
+	config['sortinghat']['sleep_for'] = '0'
+	config['sortinghat']['bots_names'] = '[Beloved Bot]'
+
+	config.add_section('panels')
+	config['panels']['kibiter_time_from'] = '"now-90d"'
+
+	config.add_section('phases')
+	config['phases']['collection'] = 'true'
+	config['phases']['identities'] = 'true'
+	config['phases']['enrichment'] = 'true'
+	config['phases']['panels'] = 'true'
+
+	config.add_section('git')
+	config['git']['raw_index'] = 'git_test-raw'
+	config['git']['enriched_index'] = 'git-test'
+
+	config.add_section('github')
+	config['github']['api-token'] = '2ba7ae1bcf2b92e0165f839e665723ac44ee223d'
+	config['github']['raw_index'] = 'github_test-raw'
+	config['github']['enriched_index'] = 'github-test'
+
+	return config
+
+
+def get_repo_url(org):
+	git = []
+	github = []
+	names = []
+	url = "https://api.github.com/users/" + org + "/repos"
+	page =0
+	last_page=0
+	r = requests.get(url + "?page=0")
+
+	while ('next' in r.links or not r.links):
+		for repo in r.json():
+			if not repo['fork']:
+				git.append(repo['clone_url'])
+				github.append(repo['html_url'])
+				names.append(repo['name'])
+		if not r.links:
+			break
+		r = requests.get(r.links['next']['url'])
+	return git, github, names
+
+def main():
+	if len(sys.argv) != 2:
+		print("Format :: python mtask1.py <orgname to explore>")
+		sys.exit(1)
+	org = sys.argv[1]
+	repos, repo_github, subprojects= get_repo_url(org)
+	config_file = create_config(org)
+	json_obj = {}
+	json_obj[org] = {}
+	json_obj[org]['git'] = repos 
+	json_obj[org]['github'] = repo_github 
+	with open(org + ".json", "w") as outfile:
+		json.dump(json_obj, outfile)
+
+	with open(org + ".cfg", "w") as conf:
+		config_file.write(conf)
+
+	# print (repos)
+	# print(repo_github)
+	# print (subprojects)
+
+if __name__ == '__main__':
+	main()
